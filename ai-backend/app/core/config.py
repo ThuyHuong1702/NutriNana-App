e617b6e3cdb4db1a56e238d291ca02faf63cb38b
ThuyHuong1702 <dothuyhuong90@gmail.com>
2026-01-07 15:41:34 +0700

    config update

--- ai-backend/app/core/config.py
+++ ai-backend/app/core/config.py
@@ -1,7 +1,30 @@
+# import os
+# from dotenv import load_dotenv
+
+# # Load biến môi trường từ file .env
+# load_dotenv()
+
+# class Settings:
+#     DB_HOST = os.getenv("DB_HOST", "localhost")
+#     DB_USER = os.getenv("DB_USER", "root")
+#     DB_PASSWORD = os.getenv("DB_PASSWORD", "admin123")
+#     DB_NAME = os.getenv("DB_NAME", "NutriNana")
+
+#     # Cấu hình kết nối cho mysql.connector
+#     DB_CONFIG = {
+#         'host': DB_HOST,
+#         'user': DB_USER,
+#         'password': DB_PASSWORD,
+#         'database': DB_NAME
+#     }
+
+# settings = Settings()
+
+#SERVE
+
 import os
 from dotenv import load_dotenv
 
-# Load biến môi trường từ file .env
 load_dotenv()
 
 class Settings:
@@ -9,13 +32,14 @@ class Settings:
     DB_USER = os.getenv("DB_USER", "root")
     DB_PASSWORD = os.getenv("DB_PASSWORD", "admin123")
     DB_NAME = os.getenv("DB_NAME", "NutriNana")
+    DB_PORT = os.getenv("DB_PORT", "3306") 
 
-    # Cấu hình kết nối cho mysql.connector
     DB_CONFIG = {
         'host': DB_HOST,
         'user': DB_USER,
         'password': DB_PASSWORD,
-        'database': DB_NAME
+        'database': DB_NAME,
+        'port': int(DB_PORT) 
     }
 
 settings = Settings()
