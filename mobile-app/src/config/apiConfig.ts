1a27c8b3229e0ed07cd9ab5a9682892e58a5dcf7
ThuyHuong1702 <dothuyhuong90@gmail.com>
2026-01-07 13:30:20 +0700

    cap nhat

--- mobile-app/src/config/apiConfig.ts
+++ mobile-app/src/config/apiConfig.ts
@@ -1,6 +1,6 @@
 // src/config/apiConfig.ts
 
-export const BACKEND_URL = 'http://192.168.1.14:8000';
+export const BACKEND_URL = 'http://172.26.144.1';
 
 // Cấu hình Google Auth
 export const GOOGLE_CLIENT_ID = '756875053212-m5fp0ld9dc0pa399a88salfokde83fjf.apps.googleusercontent.com';
